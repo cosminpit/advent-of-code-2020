02dd60e394ac643f869bf5154aab7881afe8848a
cosmin.pit <cosmin.pit@gmail.com>
2020-12-08 13:00:23 +0200

    simplify day 8

--- Day 8 - Handheld Halting/solve_part_2.py
+++ Day 8 - Handheld Halting/solve_part_2.py
@@ -1,17 +1,10 @@
-def execute(i, acc, cmd, res):
-    v = 0
+def execute(i, acc, cmd):
     if cmd[i][0] == 'nop':
-        j = i + 1
+        return [i + 1, acc]
     elif cmd[i][0] == 'jmp':
-        j = i + cmd[i][1]
+        return [i + cmd[i][1], acc]
     else:
-        j, v = i + 1, cmd[i][1]
-
-    res[i] = [False, acc + v]
-    if j not in res:
-        execute(j, acc + v, cmd, res)
-    if res[j][0]:
-        res[i] = [True, v + res[j][1]]
+        return [i + 1, acc + cmd[i][1]]
 
 with open('input.txt', 'r') as f:
     cmd = []
@@ -19,18 +12,16 @@ with open('input.txt', 'r') as f:
         a, b = line.replace('\n', '').split(' ')
         cmd.append([a, int(b)])
 
-res = {len(cmd) : [True, 0]}
-
-execute(0, 0, cmd, res)
-loop = sorted(res.keys())[:-1]
-
-for i in range(len(cmd)):
-    if i not in res:
-        execute(i, 0, cmd, res)
-
-for i in loop:
+i, acc, visited = 0, 0, set([len(cmd)])
+while True:
+    visited.add(i)
     if cmd[i][0] != 'acc':
         j = i + 1 if cmd[i][0] == 'jmp' else i + cmd[i][1]
-        if res[j][0]:
-            print(res[i][1] + res[j][1])
+        s = acc
+        while j not in visited:
+            visited.add(j)
+            j, s = execute(j, s, cmd)
+        if j == len(cmd):
+            print(s)
             break
+    i, acc = execute(i, acc, cmd)
