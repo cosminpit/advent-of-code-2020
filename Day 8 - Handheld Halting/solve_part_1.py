921971c1417aba6dfe6ecba5fd2efe7e3f16fef3
cosmin.pit <cosmin.pit@gmail.com>
2020-12-08 10:48:42 +0200

    improve to O(n)

--- Day 8 - Handheld Halting/solve_part_1.py
+++ Day 8 - Handheld Halting/solve_part_1.py
@@ -1,18 +1,18 @@
 with open('input.txt', 'r') as f:
-    cmds = []
+    cmd = []
     for line in f:
         a, b = line.replace('\n', '').split(' ')
-        cmds.append([a, int(b)])
+        cmd.append([a, int(b)])
 
 i, acc, visited = 0, 0, set([0])
 while True:
-    if cmds[i][0] == 'nop':
+    if cmd[i][0] == 'nop':
         i += 1
-    elif cmds[i][0] == 'acc':
-        acc += cmds[i][1]
+    elif cmd[i][0] == 'acc':
+        acc += cmd[i][1]
         i += 1
     else:
-        i += cmds[i][1]
+        i += cmd[i][1]
     if i in visited:
         print(acc)
         break
--- Day 8 - Handheld Halting/solve_part_2.py
+++ Day 8 - Handheld Halting/solve_part_2.py
@@ -1,38 +1,36 @@
+def execute(i, acc, cmd, res):
+    v = 0
+    if cmd[i][0] == 'nop':
+        j = i + 1
+    elif cmd[i][0] == 'jmp':
+        j = i + cmd[i][1]
+    else:
+        j, v = i + 1, cmd[i][1]
+
+    res[i] = [False, acc + v]
+    if j not in res:
+        execute(j, acc + v, cmd, res)
+    if res[j][0]:
+        res[i] = [True, v + res[j][1]]
+
 with open('input.txt', 'r') as f:
-    cmds = []
+    cmd = []
     for line in f:
         a, b = line.replace('\n', '').split(' ')
-        cmds.append([a, int(b)])
+        cmd.append([a, int(b)])
 
-def execute(cmds):
-    i, acc, visited = 0, 0, set([0])
-    while True:
-        if cmds[i][0] == 'nop':
-            i += 1
-        elif cmds[i][0] == 'acc':
-            acc += cmds[i][1]
-            i += 1
-        else:
-            i += cmds[i][1]
-        if i in visited:
-            return [False, acc]
-        elif i == len(cmds):
-            return [True, acc]
-        visited.add(i)
+res = {len(cmd) : [True, 0]}
 
-for i in range(len(cmds)):
-    c = cmds[i][0]
-    if c == 'nop':
-        cmds[i][0] = 'jmp'
-        r = execute(cmds)
-        if r[0]:
-            print(r[1])
+execute(0, 0, cmd, res)
+loop = sorted(res.keys())[:-1]
+
+for i in range(len(cmd)):
+    if i not in res:
+        execute(i, 0, cmd, res)
+
+for i in loop:
+    if cmd[i][0] != 'acc':
+        j = i + 1 if cmd[i][0] == 'jmp' else i + cmd[i][1]
+        if res[j][0]:
+            print(res[i][1] + res[j][1])
             break
-        cmds[i][0] = c
-    elif c == 'jmp':
-        cmds[i][0] = 'nop'
-        r = execute(cmds)
-        if r[0]:
-            print(r[1])
-            break
-        cmds[i][0] = c
