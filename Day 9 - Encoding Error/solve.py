85607b52b16f034a39650bb77cb28bdc6b0c2127
cosmin.pit <cosmin.pit@gmail.com>
2020-12-10 09:39:43 +0200

    implement day 9 with two pointer technique

--- Day 9 - Encoding Error/solve.py
+++ Day 9 - Encoding Error/solve.py
@@ -3,22 +3,25 @@ with open('input.txt', 'r') as f:
 
 size, x = 25, None
 for i in range(size, len(a)):
-    x = a[i]
-    for j in range(i - size, i - 1):
-        for k in range(j + 1, i):
-            if a[i] == a[j] + a[k]:
-                x = None
-                break
-    if x != None:
+    b = sorted(a[i - size:i])
+    x, p, q = a[i], 0, len(b) - 1
+    while p < q:
+        if b[p] + b[q] == x:
+            break
+        elif b[p] + b[q] > x:
+            q -= 1
+        else:
+            p += 1
+    if p == q:
         print(x)
         break
 
-s = [0 for i in range(len(a) + 1)]
+s, j = 0, 0
 for i in range(len(a)):
-    s[i + 1] = s[i] + a[i]
-
-for i in range (1, len(s)):
-    for j in range(i - 1):
-        if s[i] - s[j] == x:
-            print(max(a[j:i]) + min(a[j:i]))
-            quit()
+    while j < len(a) and s < x:
+        s += a[j]
+        j += 1
+    if s == x:
+        print(max(a[i:j]) + min(a[i:j]))
+        break        
+    s -= a[i]
